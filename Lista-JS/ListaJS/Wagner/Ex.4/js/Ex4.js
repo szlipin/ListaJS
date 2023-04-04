e0d3c086a2b2025adf59b1843081150f9e2da782
Felipe <106964514+szlipin@users.noreply.github.com>
2023-04-03 22:45:41 -0300

    Add files via upload

---
+++ Lista-JS/ListaJS/Wagner/Ex.4/js/Ex4.js
@@ -0,0 +1,12 @@
+let num1 = 0;
+let num2 = 1;
+let fibo;
+
+for (let i=2; i<15; i++) {
+
+  fibo = num1 + num2;
+  num1 = num2;
+  num2 = fibo;
+
+  document.write(fibo , " | ");
+}
---
+++ Lista-JS/ListaJS/Wagner/Ex.5/js/Ex5.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Digite um número inteiro: "));
+
+let raizQuadrada = Math.sqrt(numero);
+let raizCubica = Math.cbrt(numero);
+let potencia = Math.pow(numero, 2); // altere o valor do expoente para o que desejar
+
+alert("Raiz quadrada: " + raizQuadrada + "\nRaiz cúbica: " + raizCubica + "\nPotência: " + potencia);
+document.write("Raiz quadrada: " , raizQuadrada, "<br>" , "Raiz cúbica: " , raizCubica, "<br>" , "Potência: " , potencia);
